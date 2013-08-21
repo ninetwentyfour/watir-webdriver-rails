680cec61bd3dd61d2755fa922eff1dd916ba9c32
p5150j <p5150j@gmail.com>
2013-08-21 15:47:14 -0600

    added screenshot support, added dir path for saving pngs, added  test time date as image naming params

--- spec/integration/rails_spec.rb
+++ spec/integration/rails_spec.rb
@@ -7,5 +7,10 @@ describe "Rails startup process" do
     #browser.window.resize_to(640, 960)
     browser.text_field(:id=>"test_textbox").set("Hello")
     browser.text_field(:id=>"test_textbox").value.should == "Hello"
+    #screenshot_file = "app/assets/images/failed/screenshot-#{Time.now.strftime('%Y%m%d-%H%M%S')}.png"
+    # for rails app save screenshot files in images/failed with time-date-name.png
+    #browser.screenshot.save(screenshot_file)
+
+    # also dont forget to create a dir named 'failed' and add a .gitignore to ignore eveything in that dir exept the .gitignore :)-
   end
 end

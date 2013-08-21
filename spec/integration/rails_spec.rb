21c862c8fe1ae5b713be07156f9e97f973cd01b2
p5150j <p5150j@gmail.com>
2013-08-21 13:31:36 -0600

    added media querie support

--- spec/integration/rails_spec.rb
+++ spec/integration/rails_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe "Rails startup process" do
   it "should fill textbox" do
     browser.goto "/"
+    #use with media queries :)- 
+    #browser.window.resize_to(640, 960)
     browser.text_field(:id=>"test_textbox").set("Hello")
     browser.text_field(:id=>"test_textbox").value.should == "Hello"
   end
